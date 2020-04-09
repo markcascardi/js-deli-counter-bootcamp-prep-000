cbd4a0c04318b844ee233aecd21a9a8165cabf03
Mark Cascardi <mcascardi@gmail.com>
2020-04-09 21:32:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,88 @@
+
+function takeANumber(queue, name) {
+  queue.push(name)
+  return `Welcome, ${name}. You are number ${queue.length} in line.`
+}
+
+function nowServing(queue) {
+  if (queue.length > 0) {
+    return `Currently serving ${queue.shift()}.`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(queue) {
+  if (queue.length > 0) {
+    var userQueuePositions = []
+    for (let i = 0; i < queue.length; i++) {
+      userQueuePositions.push(`${i + 1}. ${queue[i]}`)
+    }
+    return `The line is currently: ${userQueuePositions.join(", " )}`
+  } else {
+    return "The line is currently empty."
+  }
+}
+
+
+// for ([initialization]; [condition]; [iteration]) {
+//   [loopBody];
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// function takeANumber(katzDeliLine, name) {
+//   katzDeliLine.push(name)
+//   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+// }
+//
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length > 0) {
+//   return `Currently serving ${katzDeliLine.shift()}.`
+//    } else {
+//    return `There is nobody waiting to be served!`
+//   }
+// }
+//
+// function currentLine(katzDeliLine) {
+//
+//
+//   returns `The line is currently: ${katzDeliLine[i]}. ${katzDeliLine}
+//
+//   else {
+//     returns `The line is currently empty`
+//   }
+// }
